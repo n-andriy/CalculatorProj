c1dda60caaa7bb0e700fecd97cefd5d36e8da8bd
n_andriy <n-andriy@ukr.net>
2012-05-24 08:47:08 +0300

    Roman conversion

---
+++ Calculator/MathCalcLibrary.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator
+{
+    public static class MathCalcLibrary
+    {
+        public static string ConvertIntToRoman(int number)
+        {
+            if (number < 0) return "";
+
+            // Dictionary definition of Arabic and Roman digits
+            var romanNums = new Dictionary<int, string>
+                                {
+                                    {1000, "M"},
+                                    {900, "CM"},
+                                    {500, "D"},
+                                    {400, "CD"},
+                                    {100, "C"},
+                                    {90, "XC"},
+                                    {50, "L"},
+                                    {40, "XL"},
+                                    {10, "X"},
+                                    {9, "IX"},
+                                    {5, "V"},
+                                    {4, "IV"},
+                                    {1, "I"}
+                                };
+
+            string sRtn = "";
+            int n = number;
+
+            foreach (var pair in romanNums)
+            {
+                while (n >= pair.Key)
+                {
+                    n -= pair.Key;
+                    sRtn += pair.Value;
+                }
+            }
+
+            return sRtn;
+        }
+    }
+}
---
+++ CalculatorTestProject/OperationsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTestProject
+{
+    [TestClass]
+    public class OperationsTest
+    {
+        [TestMethod]
+        public void ConvertIntToRomanNumTest()
+        {
+            Assert.AreEqual("XI", MathCalcLibrary.ConvertIntToRoman(11));
+        }
+
+        [TestMethod]
+        public void ConvertIntToRomanZeroTest()
+        {
+            Assert.AreEqual("", MathCalcLibrary.ConvertIntToRoman(0));
+        }
+    }
+}
